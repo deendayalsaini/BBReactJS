2e6c810b4a1a47e56f72be288f68652333f23b8f
deendayalsaini <deendayalsaini@gmail.com>
2018-10-02 11:41:56 +0530

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,6 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+
+module.exports = {
+  db: 'mongodb production connection string',
+  db_dev: 'mongodb development connection string',
+};
